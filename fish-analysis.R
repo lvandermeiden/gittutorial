10a7ad700f7ef17e825f5d13e1f213fe19a109b6
unknown <lvanderm21@gmail.com>
2018-10-19 11:48:18 -0500

    Add histogram with facetwrap(~length_cat)

--- fish-analysis.R
+++ fish-analysis.R
@@ -1,8 +1,13 @@
 library(tidyverse)
 fishData <- read_csv("data/Gaeta_etal_CLC_data.csv")
+source("R/themes.R")
 
 head(fishData)
 
 fishData %>% 
-  mutate(length_cat = case_when(length>300 ~ "big", length<300 ~ "small")) -> fishData
+  mutate(length_cat = case_when(length>=300 ~ "big", length<300 ~ "small")) -> fishData
 
+ggplot()+
+  geom_histogram(data=fishData, aes(x=scalelength, fill=length_cat))+
+  facet_wrap(~length_cat)+
+  theme_presentation()
