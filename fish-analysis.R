bed09a93044b7d4245773a336a79476e5e5de4cc
Christopher Chizinski <chris.chizinski@gmail.com>
2018-10-23 14:12:02 -0500

    modified plot

--- fish-analysis.R
+++ fish-analysis.R
@@ -1,7 +1,8 @@
 library(tidyverse)
-fishData <- read_csv("data/Gaeta_etal_CLC_data.csv")
+
 source("R/themes.R")
 
+fishData <- read_csv("data/Gaeta_etal_CLC_data.csv")
 head(fishData)
 
 fishData %>% 
@@ -9,5 +10,16 @@ fishData %>%
 
 ggplot()+
   geom_histogram(data=fishData, aes(x=scalelength, fill=length_cat))+
-  facet_wrap(~length_cat)+
+  facet_wrap(~length_cat) +
   theme_presentation()
+
+
+## Chris's modifications
+
+ggplot() +
+  geom_histogram(fishData = fish, aes(x = scalelength, fill = length_cat), colour = "black", bins = 25) +
+  facet_wrap(~length_cat) +
+  coord_cartesian(ylim = c(0,500), xlim = c(0, 12), expand = FALSE) +
+  theme_classic() +
+  theme(legend.position = "none",
+        panel.spacing = unit(2, "lines"))
